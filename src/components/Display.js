6f88f4b2a87f40be4ea1a60bed5f781a4907eed3
Jonas Oliveira <jloide.essence@rd.com.br>
2021-06-22 18:03:31 -0300

    estrutura do projeto

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { 
+    StyleSheet, Text
+ } from 'react-native'
+
+ export default props => {
+     return (
+
+     )
+ }
+
+const styles = StyleSheet.create({
+     
+ })
---
+++ src/screens/Calculator.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { 
+    StyleSheet, View
+ } from 'react-native'
+
+ export default class Calculator extends Component {
+
+    state = {}
+
+    render() {
+        return (
+
+        )
+    }
+ }
+
+const styles = StyleSheet.create({
+     
+})
